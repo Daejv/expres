8fcc66e3201184869c5d17e4c504c04efa42c2c4
daejv <d.buchta12@gmail.com>
2021-12-06 13:51:17 +0100

    4

--- app.js
+++ app.js
@@ -9,3 +9,21 @@ app.get('/', function(req, res) {
 app.listen(port, function() {
   console.log(`Example app listening on port ${port}!`)
 });
+
+console.log('First');
+console.log('Second');
+
+
+setTimeout(function() {
+    console.log('First');
+    }, 3000);
+ console.log('Second');
+
+ app.all('/secret', function(req, res, next) {
+    console.log('Accessing the secret section ...');
+    next(); // pass control to the next handler
+  });
+
+  const wiki = require('./wiki.js');
+// ...
+app.use('/wiki', wiki);
